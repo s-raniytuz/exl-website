06888471b7cdb4f83cadf0777692fbaeba804a1b
s-raniytuz <tuzsocial909@gmail.com>
2024-05-11 14:39:42 -0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-full w-full bg-slate-900">
+      <h1 className="text-slate-100">saranin 2040</h1>
+      <Button className="bg-slate-100 text-slate-900 hover:bg-slate-400">
+        Download
+      </Button>
+    </div>
+  );
+}
+
+export default App;
